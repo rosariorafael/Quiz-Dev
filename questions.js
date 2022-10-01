e97885e4e3f6b3cb47a94b698f24a0cbe3135160
Rafael Rosário <rafael_vlop@hotmail.com>
2022-10-01 19:20:40 -0300

    questions update

--- questions.js
+++ questions.js
@@ -12,19 +12,19 @@ var questions = [
         answer: "class"
     },
     {
-        title: "Arrays in Javascript can be used to store ____.",
-        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: "all of the above"
+        title: "É sabido que Javascript é uma linguagem de tipagem dinâmica. Isso significa que:",
+        choices: ["é sempre necessário declarar os tipos de variável antes da sua atribuição.", "os outputs funcionam automaticamente.", "as funções precisam ter um tipo na sua sintaxe de declaração.", "não é necessário declarar o tipo de variável antes de sua atribuição."],
+        answer: "não é necessário declarar o tipo de variável antes de sua atribuição."
     },
     {
-        title: "String values must be enclosed within ____ when being assigned to variables.",
-        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
-        answer: "quotes"
+        title: "Em Javascript existem diferentes tipos de operadores, marque a alternativa que contém somente operadores relacionais?",
+        choices: ["Typeof, In", "<=, >=, <<, >>", "In, Instanceof", "==, !="],
+        answer: "In, Instanceof"
     },
     {
-        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
-        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
-        answer: "console log"
+        title: "Na linguagem JavaScript, ao invocar o método getElementsByClassName, do objeto document, será retornado:",
+        choices: ["Um objeto.", "Uma função.", "Um valor numérico.", "Um array."],
+        answer: "Um array."
     },
 
 ];
